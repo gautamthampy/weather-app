6a457f050707e852f94785d5a9e69c4f0e246cf0
Gautam Thampy <thampygautam@gmail.com>
2024-05-24 21:18:09 +0530

    completed initial build, need to improve on few things

--- flutter_weather_app/lib/bloc/weather_bloc_bloc.dart
+++ flutter_weather_app/lib/bloc/weather_bloc_bloc.dart
@@ -1,13 +1,31 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_weather_app/data/my_data.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather.dart';
 
 part 'weather_bloc_event.dart';
 part 'weather_bloc_state.dart';
 
 class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
   WeatherBlocBloc() : super(WeatherBlocInitial()) {
-    on<WeatherBlocEvent>((event, emit) {
+    on<FetchWeather>((event, emit) async{
       // TODO: implement event handler
+      emit(WeatherBlocLoading());
+      try {
+        WeatherFactory wf = WeatherFactory(
+          API_KEY,
+          language: Language.ENGLISH);
+
+        Weather weather = await wf.currentWeatherByLocation(
+          event.position.latitude, 
+          event.position.longitude
+          );
+        print(weather);    
+        emit(WeatherBlocSuccess(weather));
+      } catch (e) {
+        emit(WeatherBlocFailure());
+      }
     });
   }
 }
